d2cb4a3a67027fa766dc9de57898e6c481a09274
jim.zhu <jim.zhu@ubtrobot.com>
2023-02-28 18:11:45 +0800

    * 验证demo other, 02 , 03

--- workflow/src/factory/Workflow.cc
+++ workflow/src/factory/Workflow.cc
@@ -16,233 +16,198 @@
   Author: Xie Han (xiehan@sogou-inc.com)
 */
 
+#include "Workflow.h"
+
 #include <assert.h>
 #include <stddef.h>
 #include <string.h>
-#include <utility>
+
 #include <functional>
 #include <mutex>
-#include "Workflow.h"
+#include <utility>
 
-SeriesWork::SeriesWork(SubTask *first, series_callback_t&& cb) :
-	callback(std::move(cb))
-{
-	this->queue = new SubTask *[4];  // 初始化容量为4
-	this->queue_size = 4;
-	this->front = 0;
-	this->back = 0;
-	this->in_parallel = false;    // 默认不在parallel中
-	this->canceled = false;
-	first->set_pointer(this);    // 将subTask与这个SeriesTask绑定
-	this->first = first;
-	this->last = NULL;
-	this->context = NULL;
+SeriesWork::SeriesWork(SubTask *first, series_callback_t &&cb)
+    : callback(std::move(cb)) {
+  this->queue = new SubTask *[4];  // 初始化容量为4
+  this->queue_size = 4;
+  this->front = 0;
+  this->back = 0;
+  this->in_parallel = false;  // 默认不在parallel中
+  this->canceled = false;
+  first->set_pointer(this);  // 将subTask与这个SeriesTask绑定
+  this->first = first;
+  this->last = NULL;
+  this->context = NULL;
 }
 
-void SeriesWork::dismiss_recursive()
-{
-	SubTask *task = first;
+void SeriesWork::dismiss_recursive() {
+  SubTask *task = first;
 
-	this->in_parallel = false;
-	this->callback = nullptr;
-	do
-	{
-		ParallelWork *parallel = dynamic_cast<ParallelWork *>(task);
+  this->in_parallel = false;
+  this->callback = nullptr;
+  do {
+    ParallelWork *parallel = dynamic_cast<ParallelWork *>(task);
 
-		if (parallel)
-			parallel->dismiss_recursive();
-		else
-			delete task;
+    if (parallel)
+      parallel->dismiss_recursive();
+    else
+      delete task;
 
-		task = this->pop_task();
-	} while (task);
+    task = this->pop_task();
+  } while (task);
 }
 
-void SeriesWork::expand_queue()
-{
-	int size = 2 * this->queue_size;
-	SubTask **queue = new SubTask *[size];
-	int i, j;
+void SeriesWork::expand_queue() {
+  int size = 2 * this->queue_size;
+  SubTask **queue = new SubTask *[size];
+  int i, j;
 
-	i = 0;
-	j = this->front;
-	do
-	{
-		queue[i++] = this->queue[j++];
-		if (j == this->queue_size)
-			j = 0;
-	} while (j != this->back);
+  i = 0;
+  j = this->front;
+  do {
+    queue[i++] = this->queue[j++];
+    if (j == this->queue_size) j = 0;
+  } while (j != this->back);
 
-	delete []this->queue;
-	this->queue = queue;
-	this->queue_size = size;
-	this->front = 0;
-	this->back = i;
+  delete[] this->queue;
+  this->queue = queue;
+  this->queue_size = size;
+  this->front = 0;
+  this->back = i;
 }
 
-void SeriesWork::push_front(SubTask *task)
-{
-	this->mutex.lock();
-	if (--this->front == -1)
-		this->front = this->queue_size - 1;
+void SeriesWork::push_front(SubTask *task) {
+  this->mutex.lock();
+  if (--this->front == -1) this->front = this->queue_size - 1;
 
-	task->set_pointer(this);
-	this->queue[this->front] = task;
-	if (this->front == this->back)
-		this->expand_queue();
+  task->set_pointer(this);
+  this->queue[this->front] = task;
+  if (this->front == this->back) this->expand_queue();
 
-	this->mutex.unlock();
+  this->mutex.unlock();
 }
 
-void SeriesWork::push_back(SubTask *task)
-{
-	this->mutex.lock();
-	task->set_pointer(this);
-	this->queue[this->back] = task;
-	if (++this->back == this->queue_size)
-		this->back = 0;
+void SeriesWork::push_back(SubTask *task) {
+  this->mutex.lock();
+  task->set_pointer(this);
+  this->queue[this->back] = task;
+  if (++this->back == this->queue_size) this->back = 0;
 
-	if (this->front == this->back)
-		this->expand_queue();
+  if (this->front == this->back) this->expand_queue();
 
-	this->mutex.unlock();
+  this->mutex.unlock();
 }
 
-SubTask *SeriesWork::pop()
-{
-	bool canceled = this->canceled;
-	SubTask *task = this->pop_task();
+SubTask *SeriesWork::pop() {
+  bool canceled = this->canceled;
+  SubTask *task = this->pop_task();
 
-	if (!canceled)
-		return task;
+  if (!canceled) return task;
 
-	while (task)
-	{
-		ParallelWork *parallel = dynamic_cast<ParallelWork *>(task);
+  while (task) {
+    ParallelWork *parallel = dynamic_cast<ParallelWork *>(task);
 
-		if (parallel)
-			parallel->dismiss_recursive();
-		else
-			delete task;
+    if (parallel)
+      parallel->dismiss_recursive();
+    else
+      delete task;
 
-		task = this->pop_task();
-	}
+    task = this->pop_task();
+  }
 
-	return NULL;
+  return NULL;
 }
 
-SubTask *SeriesWork::pop_task()
-{
-	SubTask *task;
+SubTask *SeriesWork::pop_task() {
+  SubTask *task;
 
-	this->mutex.lock();
-	if (this->front != this->back)
-	{
-		task = this->queue[this->front];
-		if (++this->front == this->queue_size)
-			this->front = 0;
-	}
-	else
-	{
-		task = this->last;
-		this->last = NULL;
-	}
+  this->mutex.lock();
+  if (this->front != this->back) {
+    task = this->queue[this->front];
+    if (++this->front == this->queue_size) this->front = 0;
+  } else {
+    task = this->last;
+    this->last = NULL;
+  }
 
-	this->mutex.unlock();
-	if (!task)
-	{
-		if (this->callback)
-			this->callback(this);
+  this->mutex.unlock();
+  if (!task) {
+    if (this->callback) this->callback(this);
 
-		if (!this->in_parallel)
-			delete this;
-	}
+    if (!this->in_parallel) delete this;
+  }
 
-	return task;
+  return task;
 }
 
-ParallelWork::ParallelWork(parallel_callback_t&& cb) :
-	ParallelTask(NULL, 0),
-	callback(std::move(cb))
-{
-	this->all_series = NULL;
-	this->buf_size = 0;
-	this->context = NULL;
+ParallelWork::ParallelWork(parallel_callback_t &&cb)
+    : ParallelTask(NULL, 0), callback(std::move(cb)) {
+  this->all_series = NULL;
+  this->buf_size = 0;
+  this->context = NULL;
 }
 
 ParallelWork::ParallelWork(SeriesWork *const all_series[], size_t n,
-						   parallel_callback_t&& cb) :
-	ParallelTask(new SubTask *[2 * n], n),
-	callback(std::move(cb))
-{
-	size_t i;
+                           parallel_callback_t &&cb)
+    : ParallelTask(new SubTask *[2 * n], n), callback(std::move(cb)) {
+  size_t i;
 
-	this->all_series = (SeriesWork **)&this->subtasks[n];
-	for (i = 0; i < n; i++)
-	{
-		assert(!all_series[i]->in_parallel);
-		all_series[i]->in_parallel = true;
-		this->all_series[i] = all_series[i];
-		this->subtasks[i] = all_series[i]->first;
-	}
+  this->all_series = (SeriesWork **)&this->subtasks[n];
+  for (i = 0; i < n; i++) {
+    assert(!all_series[i]->in_parallel);
+    all_series[i]->in_parallel = true;
+    this->all_series[i] = all_series[i];
+    this->subtasks[i] = all_series[i]->first;
+  }
 
-	this->buf_size = n;
-	this->context = NULL;
+  this->buf_size = n;
+  this->context = NULL;
 }
 
-void ParallelWork::expand_buf() 
-{
-	SubTask **buf;
-	size_t size;
+void ParallelWork::expand_buf() {
+  SubTask **buf;
+  size_t size;
 
-	this->buf_size *= 2;
-	if (this->buf_size == 0)
-		this->buf_size = 4;
+  this->buf_size *= 2;
+  if (this->buf_size == 0) this->buf_size = 4;
 
-	buf = new SubTask *[2 * this->buf_size];
-	size = this->subtasks_nr * sizeof (void *);
-	memcpy(buf, this->subtasks, size);
-	memcpy(buf + this->buf_size, this->all_series, size);
+  buf = new SubTask *[2 * this->buf_size];
+  size = this->subtasks_nr * sizeof(void *);
+  memcpy(buf, this->subtasks, size);
+  memcpy(buf + this->buf_size, this->all_series, size);
 
-	delete []this->subtasks;
-	this->subtasks = buf;
-	this->all_series = (SeriesWork **)&buf[this->buf_size];
+  delete[] this->subtasks;
+  this->subtasks = buf;
+  this->all_series = (SeriesWork **)&buf[this->buf_size];
 }
 
-void ParallelWork::add_series(SeriesWork *series)
-{
-	if (this->subtasks_nr == this->buf_size)
-		this->expand_buf();
+void ParallelWork::add_series(SeriesWork *series) {
+  if (this->subtasks_nr == this->buf_size) this->expand_buf();
 
-	assert(!series->in_parallel);
-	series->in_parallel = true;
-	this->all_series[this->subtasks_nr] = series;
-	this->subtasks[this->subtasks_nr] = series->first;
-	this->subtasks_nr++;
+  assert(!series->in_parallel);
+  series->in_parallel = true;
+  this->all_series[this->subtasks_nr] = series;
+  this->subtasks[this->subtasks_nr] = series->first;
+  this->subtasks_nr++;
 }
 
-void ParallelWork::dismiss_recursive()
-{
-	size_t i;
+void ParallelWork::dismiss_recursive() {
+  size_t i;
 
-	for (i = 0; i < this->subtasks_nr; i++)
-		this->all_series[i]->dismiss_recursive();
+  for (i = 0; i < this->subtasks_nr; i++)
+    this->all_series[i]->dismiss_recursive();
 
-	delete this;
+  delete this;
 }
 
-SubTask *ParallelWork::done()
-{
-	SeriesWork *series = series_of(this);
-	size_t i;
+SubTask *ParallelWork::done() {
+  SeriesWork *series = series_of(this);
+  size_t i;
 
-	if (this->callback)
-		this->callback(this);
+  if (this->callback) this->callback(this);
 
-	for (i = 0; i < this->subtasks_nr; i++)
-		delete this->all_series[i];
+  for (i = 0; i < this->subtasks_nr; i++) delete this->all_series[i];
 
-	delete this;
-	return series->pop();
+  delete this;
+  return series->pop();
 }
-
